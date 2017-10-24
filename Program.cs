04a447532c6eaf24fd9516792ae55bb818873918
lalla <juanlalla@gmail.com>
2017-10-24 19:57:46 -0300

    Primera versión de sorteo según el nombre del participante y se elige ganador utilizando Random

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sorteo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            List<string> participantes = new List<string>();  
+            Console.WriteLine("Bienvenido al sorteo!!");
+            Console.WriteLine("Ingrese a un participante");
+            string participante = Console.ReadLine();
+            while(!participante.ToLower().Equals("salir"))
+            {
+                participantes.Add(participante);
+                Console.WriteLine("Ingrese a otro participante");
+                participante = Console.ReadLine();
+            }
+
+            Console.WriteLine("Los participantes del sorteo son:");
+            foreach(string itemParticipante in participantes)
+            {
+                //ver string.Format en MSDN
+                Console.WriteLine(string.Format("-{0}", itemParticipante));
+            }
+
+            Console.WriteLine("El ganador del sorteo es...");
+            int numeroGanador = (new Random()).Next(0, participantes.Count);
+            Console.WriteLine(participantes[numeroGanador]);
+            Console.ReadKey();
+        }
+    }
+}
